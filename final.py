aa067c616d241dd23e5a114ef57d157b9e0676f6
vijayproxima <vijayj@proximabiz.com>
2024-04-04 15:24:45 +0530

    test

--- final.py
+++ final.py
@@ -38,7 +38,7 @@ thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
 invert = 255 - opening
-
+ 
 # Use Tesseract OCR to perform OCR on the image with custom configuration
 data = pytesseract.image_to_string(invert, 
                                    lang='eng', 
